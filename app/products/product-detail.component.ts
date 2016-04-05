f4470e0bb0577ea9bc5123a36a9459361921949d
fbid <federico.bidoggia@gmail.com>
2016-04-05 16:58:00 +0200

    added navigate back button

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component } from 'angular2/core'
-import { RouteParams } from 'angular2/router'
+import { RouteParams, Router } from 'angular2/router'
 
 import { IProduct } from './product'
 
@@ -11,9 +11,14 @@ export class ProductDetailComponent {
 
   pageTitle:string = 'Product Details';
 
-  constructor( private _routeParams : RouteParams){
+  constructor( private _routeParams : RouteParams,
+               private _router: Router){
     let id = +this._routeParams.get('id'); //+ converts string -> number
     this.pageTitle += `: ${id}`;
+  }
+
+  onBack(): void {
+    this._router.navigate(['Products']);
   }
 
 }
